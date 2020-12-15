2e0bb924c0ee007bbf3d3cb35bf3af6b72d22a61
Max Zeryck <71023727+mzeryck@users.noreply.github.com>
2020-12-15 12:54:15 -0800

    Additional setup bug

--- weather-cal-code.js
+++ weather-cal-code.js
@@ -18,6 +18,7 @@ const weatherCal = {
     this.bgPath = this.fm.joinPath(this.fm.libraryDirectory(), "weather-cal-" + this.name)
     this.prefPath = this.fm.joinPath(this.fm.libraryDirectory(), "weather-cal-preferences-" + name)
     this.widgetUrl = "https://raw.githubusercontent.com/mzeryck/Weather-Cal/main/weather-cal.js"
+    this.now = new Date()
     this.initialized = true
   },
 
@@ -429,7 +430,6 @@ const weatherCal = {
     this.localization = this.settings.localization
     this.format = this.settings.font
     this.data = {}
-    this.now = new Date()
     this.custom = custom
 
     if (!this.locale || this.locale == "" || this.locale == null) { this.locale = Device.locale() }
